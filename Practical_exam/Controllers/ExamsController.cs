d3f8d5b7d70a012c01b0dd39b347f0e89fe99c0b
QTruonggg <quangtruong102000@gmail.com>
2023-05-26 20:13:03 +0700

    new

--- Practical_exam/Controllers/ExamsController.cs
+++ Practical_exam/Controllers/ExamsController.cs
@@ -7,159 +7,156 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Practical_exam.Entities;
 
-namespace Practical_exam.Controllers
+namespace Practial_exam.Controllers
 {
-    public class ExamsController
+    public class ExamsController : Controller
     {
-        public class ExamsController : Controller
+        private readonly DataContext _context;
+
+        public ExamsController(DataContext context)
         {
-            private readonly DataContext _context;
+            _context = context;
+        }
 
-            public ExamsController(DataContext context)
+        // GET: Exams
+        public async Task<IActionResult> Index()
+        {
+            return _context.exams != null ?
+                        View(await _context.exams.ToListAsync()) :
+                        Problem("Entity set 'DataContext.exams'  is null.");
+        }
+
+        // GET: Exams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.exams == null)
             {
-                _context = context;
+                return NotFound();
             }
 
-            // GET: Exams
-            public async Task<IActionResult> Index()
+            var exam = await _context.exams
+                .FirstOrDefaultAsync(m => m.ExamId == id);
+            if (exam == null)
             {
-                return _context.exams != null ?
-                            View(await _context.exams.ToListAsync()) :
-                            Problem("Entity set 'DataContext.exams'  is null.");
+                return NotFound();
             }
 
-            // GET: Exams/Details/5
-            public async Task<IActionResult> Details(int? id)
+            return View(exam);
+        }
+
+        // GET: Exams/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ExamId,StartTime,ExamDate,ExamDuration")] Exam exam)
+        {
+            if (ModelState.IsValid)
             {
-                if (id == null || _context.exams == null)
-                {
-                    return NotFound();
-                }
-
-                var exam = await _context.exams
-                    .FirstOrDefaultAsync(m => m.ExamId == id);
-                if (exam == null)
-                {
-                    return NotFound();
-                }
-
-                return View(exam);
-            }
-
-            // GET: Exams/Create
-            public IActionResult Create()
-            {
-                return View();
-            }
-
-            // POST: Exams/Create
-            // To protect from overposting attacks, enable the specific properties you want to bind to.
-            // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Create([Bind("ExamId,StartTime,ExamDate,ExamDuration")] Exam exam)
-            {
-                if (ModelState.IsValid)
-                {
-                    _context.Add(exam);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index));
-                }
-                return View(exam);
-            }
-
-            // GET: Exams/Edit/5
-            public async Task<IActionResult> Edit(int? id)
-            {
-                if (id == null || _context.exams == null)
-                {
-                    return NotFound();
-                }
-
-                var exam = await _context.exams.FindAsync(id);
-                if (exam == null)
-                {
-                    return NotFound();
-                }
-                return View(exam);
-            }
-
-            // POST: Exams/Edit/5
-            // To protect from overposting attacks, enable the specific properties you want to bind to.
-            // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Edit(int id, [Bind("ExamId,StartTime,ExamDate,ExamDuration")] Exam exam)
-            {
-                if (id != exam.ExamId)
-                {
-                    return NotFound();
-                }
-
-                if (ModelState.IsValid)
-                {
-                    try
-                    {
-                        _context.Update(exam);
-                        await _context.SaveChangesAsync();
-                    }
-                    catch (DbUpdateConcurrencyException)
-                    {
-                        if (!ExamExists(exam.ExamId))
-                        {
-                            return NotFound();
-                        }
-                        else
-                        {
-                            throw;
-                        }
-                    }
-                    return RedirectToAction(nameof(Index));
-                }
-                return View(exam);
-            }
-
-            // GET: Exams/Delete/5
-            public async Task<IActionResult> Delete(int? id)
-            {
-                if (id == null || _context.exams == null)
-                {
-                    return NotFound();
-                }
-
-                var exam = await _context.exams
-                    .FirstOrDefaultAsync(m => m.ExamId == id);
-                if (exam == null)
-                {
-                    return NotFound();
-                }
-
-                return View(exam);
-            }
-
-            // POST: Exams/Delete/5
-            [HttpPost, ActionName("Delete")]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> DeleteConfirmed(int id)
-            {
-                if (_context.exams == null)
-                {
-                    return Problem("Entity set 'DataContext.exams'  is null.");
-                }
-                var exam = await _context.exams.FindAsync(id);
-                if (exam != null)
-                {
-                    _context.exams.Remove(exam);
-                }
-
+                _context.Add(exam);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            return View(exam);
+        }
 
-            private bool ExamExists(int id)
+        // GET: Exams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.exams == null)
             {
-                return (_context.exams?.Any(e => e.ExamId == id)).GetValueOrDefault();
+                return NotFound();
             }
+
+            var exam = await _context.exams.FindAsync(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            return View(exam);
+        }
+
+        // POST: Exams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ExamId,StartTime,ExamDate,ExamDuration")] Exam exam)
+        {
+            if (id != exam.ExamId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exam);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExamExists(exam.ExamId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(exam);
+        }
+
+        // GET: Exams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.exams == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.exams
+                .FirstOrDefaultAsync(m => m.ExamId == id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return View(exam);
+        }
+
+        // POST: Exams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.exams == null)
+            {
+                return Problem("Entity set 'DataContext.exams'  is null.");
+            }
+            var exam = await _context.exams.FindAsync(id);
+            if (exam != null)
+            {
+                _context.exams.Remove(exam);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExamExists(int id)
+        {
+            return (_context.exams?.Any(e => e.ExamId == id)).GetValueOrDefault();
         }
     }
 }
